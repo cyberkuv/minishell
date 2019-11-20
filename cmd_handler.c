f2567b04387868940807ddc02d708882525c8170
Emmanuel Onwe <eonwe@student.wethinkcode.co.za>
2019-11-20 16:08:17 +0200

    env done

--- cmd_handler.c
+++ cmd_handler.c
@@ -3,7 +3,10 @@
 int     cmd_handler(char **parsed)
 {
     int NoOfOwnCmds = 4, i, switchOwnArg = 0;
-    char* ListOfOwnCmds[NoOfOwnCmds];
+    char *ListOfOwnCmds[NoOfOwnCmds];
+    char *user = getenv("USER");
+    char *shell = getenv("SHELL");
+    char *home = getenv("HOME");
     ListOfOwnCmds[0] = "exit"; 
     ListOfOwnCmds[1] = "cd"; 
     ListOfOwnCmds[2] = "echo"; 
@@ -11,7 +14,7 @@ int     cmd_handler(char **parsed)
     for (i = 0; i < NoOfOwnCmds; i++) { 
         if (ft_strcmp(parsed[0], ListOfOwnCmds[i]) == 0) { 
             switchOwnArg = i + 1; 
-            break; 
+            break;
         } 
     } 
     switch (switchOwnArg) { 
@@ -21,9 +24,12 @@ int     cmd_handler(char **parsed)
     case 2: 
         chdir(parsed[1]); 
         return 1; 
-    case 3: 
+    case 3:
         return 1;
     case 4:
+        ft_printf("\x1B[34m@USER=%s\x1B[0m\n", user);
+        ft_printf("\x1B[34m@SHELL=%s\x1B[0m\n", shell);
+        ft_printf("\x1B[34m@HOME=%s\x1B[0m\n", home);
         return 1; 
     default:
         break; 
